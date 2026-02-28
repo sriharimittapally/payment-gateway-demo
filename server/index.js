e1b2a62a12b70052a6bd79248bc97a1b795906a5
sriharimittapally <sriharimittapally95@gmail.com>
2026-02-28 19:48:27 +0530

    index-updated

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://payment-gateway-demo-frontend.vercel.app",
     methods: ["GET", "POST", "OPTIONS"],
     allowedHeaders: ["Content-Type"],
   })
